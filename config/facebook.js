35b296ccff21603c3dd51a090aa5bb8a0d707e1c
Dilip <dilipchouhan133@gmail.com>
2017-04-17 23:12:50 +0530

    Feature: FACEBOOK_REDIRECT_URI as env

--- config/facebook.js
+++ config/facebook.js
@@ -1,7 +1,7 @@
 const authCredentials = {
   'clientID'      : '143274286208393',
   'clientSecret'  : '3136009aa7f0ff38606de3bbc053de55',
-  'callbackURL'   : 'http://localhost:4000/auth/facebook/callback'
+  'callbackURL'   : precess.env.FACEBOOK_REDIRECT_URI || 'http://localhost:4000/auth/facebook/callback'
 };
 
 export default authCredentials;
